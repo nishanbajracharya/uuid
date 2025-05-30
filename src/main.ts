c0b46055fcaeb305cd4b8dde55f7a26753a66bad
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2025-05-30 14:04:55 +0545

    Add copy function

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,26 @@ function generateNewUUID(element: HTMLElement) {
 
 generateNewUUID(uuidValueElement);
 
+const copyButtonElement = document.querySelector<HTMLButtonElement>('#copy-button')!;
+
+copyButtonElement.addEventListener('click', async () => {
+  const value = uuidValueElement.innerHTML;
+
+  await copyText(value);
+})
+
+
 const generateButtonElement = document.querySelector<HTMLButtonElement>('#generate-button')!;
 
 generateButtonElement.addEventListener('click', () => {
   generateNewUUID(uuidValueElement);
 });
+
+async function copyText(text: string) {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Text copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy text: ', err);
+  }
+}
