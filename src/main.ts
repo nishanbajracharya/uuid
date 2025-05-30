4d1edbce3951c39714e1e4d5f163a32766e91bf8
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2025-05-30 14:01:56 +0545

    Add method to generate uuid

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import 'modern-normalize/modern-normalize.css';
 
 import './assets/styles/base.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+import * as uuid from 'uuid';
 
-app.innerHTML = 'Hello World';
+const uuidValueElement = document.querySelector<HTMLElement>('#uuid-value')!;
+
+function generateNewUUID(element: HTMLElement) {
+  element.innerHTML = uuid.v4();
+}
+
+generateNewUUID(uuidValueElement);
+
+const generateButtonElement = document.querySelector<HTMLButtonElement>('#generate-button')!;
+
+generateButtonElement.addEventListener('click', () => {
+  generateNewUUID(uuidValueElement);
+});
